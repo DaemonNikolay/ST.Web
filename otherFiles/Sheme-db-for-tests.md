881203da7e82732d0859d39135439b0c9abb337d
DaemonNikolay <nikolay-eckert@mail.ru>
2018-10-31 11:20:01 +0700

    Append project from Bitbucket

---
+++ otherFiles/Sheme-db-for-tests.md
@@ -0,0 +1,47 @@
+# Схема таблиц базы данных для проведения тестирования
+
+## Test
+
+1. id (PK, integer);
+2. name (string 50, required, unique);
+3. is_visible (bool[false], required);
+4. type (enum['standard', 'selection k of n'], required);
+5. approve (bool[false], required);
+6. random_select (bool[false], required);
+
+## Question
+
+1. id (PK, integer);
+2. title (string 100, required);
+3. descriptrion (text, required);
+4. answers (FK->*Answer_for_question{question_id}*, required);
+5. type (enum['single', 'multiple', 'its response', 'drop-down menu'], required);
+6. order (integer);
+7. group (FK->*Group_question*);
+
+## Group_question
+
+1. id (PK, integer);
+2. name (string 50, required, unique);
+
+## Answer_for_question
+
+1. id (PK, integer);
+2. question_id (FK->*Question*, required);
+3. answer (text, required);
+4. true (bool[false]);
+
+## User_answer_test
+
+1. id (PK, integer);
+2. question_id (FK->*Question*, required);
+3. answer (text, required);
+
+## Point_for_test
+
+1. id (PK, integer);
+2. user_id (FK->*User*, required);
+3. test_id(FK->*Test*, required);
+4. mark (integer, required);
+5. percent_true_answers (integer, required);
+6. percent_false_answers (integer, required);
---
+++ views/layouts/main.php
@@ -0,0 +1,109 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use app\widgets\Alert;
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use app\assets\AppAsset;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+        (function (d, w, c) {
+            (w[c] = w[c] || []).push(function() {
+                try {
+                    w.yaCounter50926319 = new Ya.Metrika2({
+                        id:50926319,
+                        clickmap:true,
+                        trackLinks:true,
+                        accurateTrackBounce:true
+                    });
+                } catch(e) { }
+            });
+
+            var n = d.getElementsByTagName("script")[0],
+                s = d.createElement("script"),
+                f = function () { n.parentNode.insertBefore(s, n); };
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = "https://mc.yandex.ru/metrika/tag.js";
+
+            if (w.opera == "[object Opera]") {
+                d.addEventListener("DOMContentLoaded", f, false);
+            } else { f(); }
+        })(document, window, "yandex_metrika_callbacks2");
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/50926319" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+<div class="wrap">
+    <?php
+    NavBar::begin([
+        'brandLabel' => Yii::$app->name,
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar-inverse navbar-fixed-top',
+        ],
+    ]);
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav navbar-right'],
+        'items' => [
+            ['label' => 'Home', 'url' => ['/site/index']],
+            ['label' => 'About', 'url' => ['/site/about']],
+            ['label' => 'Contact', 'url' => ['/site/contact']],
+            Yii::$app->user->isGuest ? (
+                ['label' => 'Login', 'url' => ['/site/login']]
+            ) : (
+                '<li>'
+                . Html::beginForm(['/site/logout'], 'post')
+                . Html::submitButton(
+                    'Logout (' . Yii::$app->user->identity->username . ')',
+                    ['class' => 'btn btn-link logout']
+                )
+                . Html::endForm()
+                . '</li>'
+            )
+        ],
+    ]);
+    NavBar::end();
+    ?>
+
+    <div class="container">
+        <?= Breadcrumbs::widget([
+            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+        ]) ?>
+        <?= Alert::widget() ?>
+        <?= $content ?>
+    </div>
+</div>
+
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+
+        <p class="pull-right"><?= Yii::powered() ?></p>
+    </div>
+</footer>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
